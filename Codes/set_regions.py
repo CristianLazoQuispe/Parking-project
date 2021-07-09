ae4284e6fe07f5f270afa331b0ec832124b5f1f4
CristianLazoQuispe <mecatronico.lazo@gmail.com>
2021-07-09 17:37:17 -0500

    plot spaces

--- Codes/set_regions.py
+++ Codes/set_regions.py
@@ -1,15 +1,15 @@
-import os
-import numpy as np
-import cv2
-import pickle
 import argparse
+import os
+import pickle
+
+import cv2
 import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.collections import PatchCollection
 from matplotlib.patches import Polygon
 from matplotlib.widgets import PolygonSelector
-from matplotlib.collections import PatchCollection
-from shapely.geometry import box
 from shapely.geometry import Polygon as shapely_poly
-
+from shapely.geometry import box
 
 points = []
 prev_points = []
@@ -17,7 +17,17 @@ patches = []
 total_points = []
 breaker = False
 
-# https://stackoverflow.com/questions/60724263/python-drawing-a-polygon-over-webcam-video-using-mouse-clicks-to-detect-points
+
+COLOR_PURPLE = (130,40,76)
+COLOR_GREEN  = (20,255,57)
+COLOR_BLACK  = (0,0,0)
+COLOR_RED    = (0,0,255)
+
+# COLOR TO PLOT
+COLOR_SPACE_FREE     = COLOR_GREEN 
+COLOR_SPACE_NOT_FREE = COLOR_RED 
+COLOR_EDGES = COLOR_BLACK
+
 
 
 class SelectFromCollection(object):
@@ -82,41 +92,130 @@ def get_image_from_video(video_path,n_frame = 5):
 
     return cv2.cvtColor(rgb_image,cv2.COLOR_BGR2RGB)
 
+def read_points(filename):
+    '''
+    Input:
+         - filename: str
+    Output:
+        - data : array of points
+    '''
+    print("Reading : ",filename)
+    data = None
+    with open(filename,'rb') as f:
+        data = pickle.load(f)
+        print("File ok ")
+    return data
+
+
+def plot_points(image,points,states_is_free,alpha=0.1):
+    '''
+    Input:
+         - image: BGR matrix [:,:,3]
+         - points: arrays of points 
+            [ [[x1,y1],[x2,y2],...,[xn,yn]],
+              [[x1,y1],[x2,y2],...,[xn,yn]],
+              ..,
+              [[x1,y1],[x2,y2],...,[xn,yn]]
+            ]
+        - states_is_free: arrays of boolean states
+            True : is free
+            False: not is free
+        - alpha: int
+          number of transparent overlays 
+    Output:
+        - image : BGR matrix with spaces
+
+    '''
+    output_image = image.copy()
+
+    for contours,state_is_free in zip(points,states_is_free):
+        if state_is_free:
+            image = cv2.fillPoly(image, pts =[contours], color=COLOR_SPACE_FREE)
+        else:
+            image = cv2.fillPoly(image, pts =[contours], color=COLOR_SPACE_NOT_FREE)
+        image = cv2.polylines(image, pts =[contours], isClosed=True, color=COLOR_EDGES, thickness=2)
+
+    output_image = cv2.addWeighted(image, alpha, output_image, 1 - alpha,0, output_image)
+
+    return output_image
+
+
+
 if __name__ == '__main__':
+
     parser = argparse.ArgumentParser()
-    parser.add_argument('video_path', help="Path of video file", default="../Data/stace_park3.mp4")
-    parser.add_argument('--out_file', help="Name of the output file", default="../Results/regions.p")
+    parser.add_argument("-v",'--video_path', help="Path of video file", default="../Data/stace_park3.mp4",type=str)
+    parser.add_argument("-f",'--n_frame', help="Number of frame to process", default=5,type=int)
+    parser.add_argument("-o",'--out_file', help="Name of the output file", default="../Results/regions.p",type=str)
     args = parser.parse_args()
 
     global globSelect
     global savePath
+
+    # Define parameters
     savePath = args.out_file if args.out_file.endswith(".p") else args.out_file+".p"
-
-    print("\n> Select a region in the figure by enclosing them within a quadrilateral.")
-    print("> Press the 'f' key to go full screen.")
-    print("> Press the 'esc' key to discard current quadrilateral.")
-    print("> Try holding the 'shift' key to move all of the vertices.")
-    print("> Try holding the 'ctrl' key to move a single vertex.")
-    print("> After marking a quadrilateral press 'n' to save current quadrilateral and then press 'q' to start marking a new quadrilateral")
-    print("> When you are done press 'b' to Exit the program\n")
+    n_frame = args.n_frame
+    video_path = args.video_path
+    out_file = args.out_file
     
-    print(args.video_path)
-    rgb_image = get_image_from_video(args.video_path,n_frame = 5)
 
-    cv2.destroyAllWindows()
+    print("Path of video file         : ",args.video_path)
+    print("Number of frame to process : ",args.n_frame)
+    print("Name of the output file    : ",args.out_file)
 
-    while True:
-        fig, ax = plt.subplots()
-        image = rgb_image
-        ax.imshow(image)
-    
-        p = PatchCollection(patches, alpha=0.7)
-        p.set_array(10*np.ones(len(patches)))
-        ax.add_collection(p)
-            
-        globSelect = SelectFromCollection(ax)
-        bbox = plt.connect('key_press_event', onkeypress)
-        break_event = plt.connect('key_press_event', break_loop)
-        print(bbox)
-        plt.show()
-        globSelect.disconnect()
+
+    # Check if the points exits
+    if os.path.exists(savePath):
+        print('File already exists :',savePath)
+
+        # get image 
+        rgb_image = get_image_from_video(video_path,n_frame = n_frame)
+        bgr_image = cv2.cvtColor(rgb_image,cv2.COLOR_RGB2BGR).copy()
+
+        # get points        
+        points = read_points(out_file)
+        
+        # simulate states
+        states_is_free = [True for i in range(len(points))]
+
+        # plot points and states
+        output_image = plot_points(bgr_image,points,states_is_free,0.2)
+
+        # show image
+        cv2.imshow("Plot parking spaces ", output_image)
+        cv2.waitKey()
+
+    else:
+        print(10*'*','Make points',10*'*')
+        print("\n> Select a region in the figure by enclosing them within a quadrilateral.")
+        print("> Press the 'f' key to go full screen.")
+        print("> Press the 'esc' key to discard current quadrilateral.")
+        print("> Try holding the 'shift' key to move all of the vertices.")
+        print("> Try holding the 'ctrl' key to move a single vertex.")
+        print("> After marking a quadrilateral press 'n' to save current quadrilateral and then press 'q' to start marking a new quadrilateral")
+        print("> When you are done press 'b' to Exit the program\n")
+        
+
+        rgb_image = get_image_from_video(video_path,n_frame = n_frame)
+
+        cv2.destroyAllWindows()
+
+        while True:
+            fig, ax = plt.subplots()
+            image = rgb_image
+            ax.imshow(image)
+        
+            p = PatchCollection(patches, alpha=0.7)
+            p.set_array(10*np.ones(len(patches)))
+            ax.add_collection(p)
+                
+            globSelect = SelectFromCollection(ax)
+            bbox = plt.connect('key_press_event', onkeypress)
+            break_event = plt.connect('key_press_event', break_loop)
+            print(bbox)
+            manager = plt.get_current_fig_manager()
+            manager.window.showMaximized()
+
+            plt.show()
+            globSelect.disconnect()
+        
